09d916a6addee22733e756faf499bc01796f992b
Lars-Erik Bruce <lars-erik@tripletex.no>
2020-01-03 14:55:00 +0100

    Add lookup of postal numbers for municipality.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import orgnr from './orgnr.js';
 import kontonr from './kontonr.js';
+import postnummer from './postal.js'
 
 
 function newOrgNr() {
@@ -7,8 +8,20 @@ function newOrgNr() {
 	orgSpan.innerText = orgnr();
 }
 
+
 function newKontoNr() {
 	document.querySelector('.js-kontonr').innerText = kontonr();
+}
+
+function queryPostal(e) {
+	const value = e.target.value;
+	if(!value || value.length !== 4) {
+		document.querySelector('[name="kommune"]').value = '';
+		return;
+	}
+	const postalCode = parseInt(value);
+	const municipality = postnummer(postalCode);
+	document.querySelector('[name="kommune"]').value = municipality || '';
 }
 
 newOrgNr();
@@ -16,4 +29,4 @@ newKontoNr();
 
 document.querySelector('.js-gen-orgnr').addEventListener('click', newOrgNr);
 document.querySelector('.js-gen-kontonr').addEventListener('click', newKontoNr);
-
+document.querySelector('[name="postnummer"]').addEventListener('input', queryPostal);
