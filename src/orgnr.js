93dd4108dc7118717a2b9d25ecc739136c340e3d
Tanet Trimas <tanet@trimas.no>
2020-05-30 23:57:37 +0200

    refactor: use mod11_generate instead of reimplement logic

--- src/orgnr.js
+++ src/orgnr.js
@@ -1,16 +1,14 @@
-import { mod11 } from './mod11.js'
+import { mod11_generate } from './mod11.js'
 
 export default function orgnr() {
-	const n = () => Math.floor(Math.random()*10);
-	const num1 = (Math.floor(Math.random()*9) + 1); // special case; cannot be 0
-	const baseOrgNumbers = [num1]
-	for (let i = 0; i < 7; i++) {
-		baseOrgNumbers.push(n())
-	}
+	const orgNummer = mod11_generate(8);
 
-	const controlCipher = mod11(baseOrgNumbers)
-	if (controlCipher === '-') {
+	const firstNumber = parseInt(orgNummer[0]);
+	const controlCipher = orgNummer[orgNummer.length - 1];
+
+	if (controlCipher === '-' || firstNumber === 0) {
+		console.log('invalid orgnr')
 		return orgnr()
 	}
-	return baseOrgNumbers.join('') + controlCipher
+	return orgNummer
 }
