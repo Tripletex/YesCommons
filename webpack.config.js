745a636e05cdafdc495ac01685255fcc13e177f5
Henrik Klev <henrik.klev@visma.com>
2021-06-04 17:21:37 +0200

    Added typescript to project

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,17 @@ module.exports = {
             localesToKeep: ['es-us', 'nb-no'],
         }),
     ],
-    devtool: 'source-map'
+    devtool: 'source-map',
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { test: /\.js$/, loader: "source-map-loader" },
+        ],
+    },
 };
