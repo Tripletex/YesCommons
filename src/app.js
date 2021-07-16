9eca697452fbb66e41171a24a655a725bbf7b51a
Tanet <tanet@trimas.no>
2021-07-16 13:20:11 +0200

    Generate accountnumber based on registernumber
    Adds a select to the page, fetches the jsonfile with bankregisterentries
    and when the user clicks generate it uses that registernumber to create a
    bankaccountnumber

--- src/app.js
+++ src/app.js
@@ -1,119 +1,136 @@
-import orgnr from './services/orgnr';
-import { generateAccountNumber, validateAccountNumber } from './services/kontonr';
-import postnummer from './services/postal'
-import { generateKid, validateKid } from './services/kid'
-import { generateFnr, validateFnr } from './services/fnr'
-import getFnrType from './helpers/fnr/getFnrType' 
+import orgnr from "./services/orgnr";
+import { generateAccountNumber, validateAccountNumber } from "./services/kontonr";
+import postnummer from "./services/postal";
+import { generateKid, validateKid } from "./services/kid";
+import { generateFnr, validateFnr } from "./services/fnr";
+import getFnrType from "./helpers/fnr/getFnrType";
 
 function newKidNr() {
-	try {
-		const RANDOM_KID_NUMBER_LENGTH = 3 + Math.floor(Math.random() * 22)
-		const kidNumberSpan = document.querySelector('#kidNumberBase')
-		const kidNumberLengthSpan = document.querySelector('#kidNumberLength')
-		const kidNumberLength = kidNumberLengthSpan.value.trim()
-		const kidNumberBase = kidNumberSpan.value.trim()
-		const modulo = document.querySelector('select#modulo_generate').value;
-		const kidNumber = !kidNumberBase && !kidNumberLength 
-		? generateKid({ length: RANDOM_KID_NUMBER_LENGTH, modulo }) 
-		: !kidNumberBase && kidNumberLength 
-		? generateKid({ length: kidNumberLength, modulo })
-		: generateKid({ length: kidNumberLength, baseNum: kidNumberBase, modulo })
-		document.querySelector('.js-kid-nummer').innerText = kidNumber
-	} catch(e) {
-		document.querySelector('.js-kid-nummer').innerText = e.message
-	}
+    try {
+        const RANDOM_KID_NUMBER_LENGTH = 3 + Math.floor(Math.random() * 22);
+        const kidNumberSpan = document.querySelector("#kidNumberBase");
+        const kidNumberLengthSpan = document.querySelector("#kidNumberLength");
+        const kidNumberLength = kidNumberLengthSpan.value.trim();
+        const kidNumberBase = kidNumberSpan.value.trim();
+        const modulo = document.querySelector("select#modulo_generate").value;
+        const kidNumber =
+            !kidNumberBase && !kidNumberLength
+                ? generateKid({ length: RANDOM_KID_NUMBER_LENGTH, modulo })
+                : !kidNumberBase && kidNumberLength
+                ? generateKid({ length: kidNumberLength, modulo })
+                : generateKid({
+                      length: kidNumberLength,
+                      baseNum: kidNumberBase,
+                      modulo,
+                  });
+        document.querySelector(".js-kid-nummer").innerText = kidNumber;
+    } catch (e) {
+        document.querySelector(".js-kid-nummer").innerText = e.message;
+    }
 }
 
 function newFnr() {
-	const fnrSpan = document.querySelector('.js-fnr');
-	const fnrType = document.querySelector('select#fnr_type').value;
-	const { fnr } = generateFnr({ fnrType });
-	fnrSpan.innerText = fnr;
+    const fnrSpan = document.querySelector(".js-fnr");
+    const fnrType = document.querySelector("select#fnr_type").value;
+    const { fnr } = generateFnr({ fnrType });
+    fnrSpan.innerText = fnr;
 }
 
 function newOrgNr() {
-	const orgSpan = document.querySelector('.js-orgnr');
-	orgSpan.innerText = orgnr();
+    const orgSpan = document.querySelector(".js-orgnr");
+    orgSpan.innerText = orgnr();
 }
 
-
 function newKontoNr() {
-	const kontoNummer = generateAccountNumber()
-	document.querySelector('.js-kontonr').innerText = kontoNummer;
+    const bankRegistrySelect = document.getElementById("bankregistry");
+    const selectedRegistryNumber =
+        bankRegistrySelect.options[bankRegistrySelect.options.selectedIndex].value;
+    const { number } = generateAccountNumber(selectedRegistryNumber);
+    document.querySelector(".js-kontonr").innerText = number;
 }
 
 function validateKontoNr(e) {
-	e.preventDefault();
-	const accountNumberInput = document.querySelector('input#accountNumber');
-	const resultSpan = document.querySelector('span#accont_validation_result');
-	const initialAccountNumber = '' + accountNumberInput.value;
-	const accountNumber = accountNumberInput.value.replace(/\s+/g, '');
-	if (accountNumber.length !== 11) {
-		resultSpan.textContent = 'Et gyldig norsk kontonummer har kun 11 siffer'
-		accountNumberInput.value = '';
-		return;
-	}
-	const isValidAccountNumber = validateAccountNumber(accountNumber);
-	resultSpan.textContent = `${initialAccountNumber} er et ${isValidAccountNumber ? 'gyldig' : 'ugyldig'} kontonummer.`;
-	accountNumberInput.value = '';
+    e.preventDefault();
+    const accountNumberInput = document.querySelector("input#accountNumber");
+    const resultSpan = document.querySelector("span#accont_validation_result");
+    const initialAccountNumber = "" + accountNumberInput.value;
+    const accountNumber = accountNumberInput.value.replace(/\s+/g, "");
+    if (accountNumber.length !== 11) {
+        resultSpan.textContent = "Et gyldig norsk kontonummer har kun 11 siffer";
+        accountNumberInput.value = "";
+        return;
+    }
+    const isValidAccountNumber = validateAccountNumber(accountNumber);
+    resultSpan.textContent = `${initialAccountNumber} er et ${
+        isValidAccountNumber ? "gyldig" : "ugyldig"
+    } kontonummer.`;
+    accountNumberInput.value = "";
 }
 
 function validateKidNr(e) {
-	e.preventDefault();
-	try {
-		const kidNumberInput = document.querySelector('input#kidNumberValidation');
-		const resultSpan = document.querySelector('span#kidr_validation_result');
-		const initialKidNumber = '' + kidNumberInput.value;
-		const kidNumber = kidNumberInput.value.replace(/\s+/g, '');
-		const modulo = document.querySelector('select#modulo_validate').value;
-		const isValidKidNr = validateKid(kidNumber, modulo);
-		resultSpan.textContent = `${initialKidNumber} er et ${isValidKidNr ? 'gyldig' : 'ugyldig'} kidnummer.`;	
-	} catch (error) {
-		resultSpan.textContent = error.message;
-	}
+    e.preventDefault();
+    try {
+        const kidNumberInput = document.querySelector("input#kidNumberValidation");
+        const resultSpan = document.querySelector("span#kidr_validation_result");
+        const initialKidNumber = "" + kidNumberInput.value;
+        const kidNumber = kidNumberInput.value.replace(/\s+/g, "");
+        const modulo = document.querySelector("select#modulo_validate").value;
+        const isValidKidNr = validateKid(kidNumber, modulo);
+        resultSpan.textContent = `${initialKidNumber} er et ${
+            isValidKidNr ? "gyldig" : "ugyldig"
+        } kidnummer.`;
+    } catch (error) {
+        resultSpan.textContent = error.message;
+    }
 }
 
 function validateFnrForm(e) {
-	e.preventDefault();
-	const fNrInput = document.querySelector('input#fnr_validation');
-	const resultSpan = document.querySelector('span#fnr_validation_result');
-	const initialFnr = '' + fNrInput.value;
-	const fnrToCheck = initialFnr.replace(/\s+/g, '');
-	const fnrType = getFnrType(fnrToCheck)
-	const { success: isValidFnr, fnr, typeString, ...rest} = validateFnr(fnrToCheck, fnrType)
-	if (!isValidFnr) {
-		resultSpan.textContent = `${fnrToCheck} er ugyldig.`;
-		fNrInput.value = ''
-		return
-	}
-	fNrInput.value = ''
-	resultSpan.textContent = `${fnrToCheck} er et gyldig ${typeString}.`;
-	if (rest.gender) {
-		// This property is available if it is not a FH-number
-		resultSpan.textContent += ` Dette ${typeString}et tilhører en ${rest.gender === 'male' ? 'mann' : 'kvinne'} som er født den ${rest.date} `
-	}	
+    e.preventDefault();
+    const fNrInput = document.querySelector("input#fnr_validation");
+    const resultSpan = document.querySelector("span#fnr_validation_result");
+    const initialFnr = "" + fNrInput.value;
+    const fnrToCheck = initialFnr.replace(/\s+/g, "");
+    const fnrType = getFnrType(fnrToCheck);
+    const {
+        success: isValidFnr,
+        fnr,
+        typeString,
+        ...rest
+    } = validateFnr(fnrToCheck, fnrType);
+    if (!isValidFnr) {
+        resultSpan.textContent = `${fnrToCheck} er ugyldig.`;
+        fNrInput.value = "";
+        return;
+    }
+    fNrInput.value = "";
+    resultSpan.textContent = `${fnrToCheck} er et gyldig ${typeString}.`;
+    if (rest.gender) {
+        // This property is available if it is not a FH-number
+        resultSpan.textContent += ` Dette ${typeString}et tilhører en ${
+            rest.gender === "male" ? "mann" : "kvinne"
+        } som er født den ${rest.date} `;
+    }
 }
 
 function queryPostal(e) {
-	const value = e.target.value;
-	if(!value || value.length !== 4) {
-		document.querySelector('[name="kommune"]').value = '';
-		return;
-	}
-	const postalCode = parseInt(value);
-	const municipality = postnummer(postalCode);
-	document.querySelector('[name="kommune"]').value = municipality || '';
+    const value = e.target.value;
+    if (!value || value.length !== 4) {
+        document.querySelector('[name="kommune"]').value = "";
+        return;
+    }
+    const postalCode = parseInt(value);
+    const municipality = postnummer(postalCode);
+    document.querySelector('[name="kommune"]').value = municipality || "";
 }
 
 newOrgNr();
-newKontoNr();
 newFnr();
 
-document.querySelector('.js-gen-orgnr').addEventListener('click', newOrgNr);
-document.querySelector('.js-gen-fnr').addEventListener('click', newFnr);
-document.querySelector('.js-gen-kid').addEventListener('click', newKidNr);
-document.querySelector('.js-gen-kontonr').addEventListener('click', newKontoNr);
-document.querySelector('.js-val-kontonr').addEventListener('click', validateKontoNr);
-document.querySelector('.js-val-kidnr').addEventListener('click', validateKidNr);
-document.querySelector('.js-val-fnr').addEventListener('click', validateFnrForm);
-document.querySelector('[name="postnummer"]').addEventListener('input', queryPostal);
+document.querySelector(".js-gen-orgnr").addEventListener("click", newOrgNr);
+document.querySelector(".js-gen-fnr").addEventListener("click", newFnr);
+document.querySelector(".js-gen-kid").addEventListener("click", newKidNr);
+document.querySelector(".js-gen-kontonr").addEventListener("click", newKontoNr);
+document.querySelector(".js-val-kontonr").addEventListener("click", validateKontoNr);
+document.querySelector(".js-val-kidnr").addEventListener("click", validateKidNr);
+document.querySelector(".js-val-fnr").addEventListener("click", validateFnrForm);
+document.querySelector('[name="postnummer"]').addEventListener("input", queryPostal);
--- src/services/kontonr.js
+++ src/services/kontonr.js
@@ -1,14 +1,36 @@
-import { mod11_generate, mod11_validate } from '../lib/mod/mod11'
+import { mod11_generate, mod11_validate } from "../lib/mod/mod11";
+import gen from "../helpers/gen";
+import bankRegistry from "../../bankregistry.json";
 
 /**
- * @returns {string} The calculated accountNumber as a string
+ * @returns {{
+ *    number: string
+ *    bic: string
+ *    bank: string
+ *    registryNumber: string
+ * }} The calculated accountNumber as a string
  */
-export function generateAccountNumber() {
-  const accountNumber = mod11_generate(10);
-  if (accountNumber[accountNumber.length-1] === '-') {
-    return generateAccountNumber();
-  }
-  return accountNumber;
+export function generateAccountNumber(registryNumber) {
+    const registryNumberEntriesAsNumber = registryNumber
+        .split("")
+        .map((n) => parseInt(n));
+    const accountNumber = mod11_generate(10, [
+        ...registryNumberEntriesAsNumber,
+        ...gen(6),
+    ]);
+    if (accountNumber[accountNumber.length - 1] === "-") {
+        return generateAccountNumber(registryNumber);
+    }
+    const bankRegisterNumber = accountNumber.slice(0, 4);
+    const registryEntry = bankRegistry.find(
+        (registry) => registry["Bank identifier"] === bankRegisterNumber
+    );
+    return {
+        number: accountNumber,
+        bic: registryEntry.BIC,
+        bank: registryEntry.Bank,
+        registryNumber: registryEntry["Bank identifier"],
+    };
 }
 
 /**
@@ -17,5 +39,5 @@ export function generateAccountNumber() {
  * @returns Boolean value to see if the checkDigit matches the last cipher in the accountNumber
  */
 export function validateAccountNumber(accountNumber) {
-  return mod11_validate(accountNumber)
+    return mod11_validate(accountNumber);
 }
